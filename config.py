6489a15e6e7453a39792be4c89db8c5170b057c5
CHuang <chuang@zscaler.com>
2017-08-11 21:40:49 -0700

    Change some file names

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 CONFIG_DIR = '../config/'
 DATA_DIR = '../data/'
-TRADABLE_STOCKS = '../data/tradable_stocks.csv'
-CUR_SYM = '../data/cur_symbol.csv'
+TRADABLE_STOCKS = '../data/tradable_stocks.txt'
+CUR_SYM = '../data/cur_symbol.txt'
