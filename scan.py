99a749998b1f82da1ee9f241a1f7066c31728b3f
cyhscg2015 <cyhscg2015@gmail.com>
2017-10-17 05:16:02 -0700

    Add scan for percentage movers

--- scan.py
+++ scan.py
@@ -2,12 +2,14 @@ import sys
 import os
 import json
 import pandas as pd
+import time
 import config
 from quote import Quote
 from analysis import Analysis
 from renko_pattern import RenkoPatterns
 from finviz import Finviz
 from report import Report
+from yahoo import Yahoo
 
 DATA_DIR = config.DATA_DIR
 ANALYSIS_DIR = config.ANALYSIS_DIR
@@ -35,6 +37,7 @@ class Scan:
 
         for sym in sym_list:
             df = q.update(sym, latest_date)
+            time.sleep(1)
  
     def update_analysis(self, symbol_list = None):
 
@@ -115,3 +118,72 @@ class Scan:
         print len(symbol_list)
 
         return symbol_list
+
+    def percent_change_one_symbol(self, sym, pc, vr):
+        y = Yahoo()
+        res = y.basic(sym)
+        percent_change = res['percent_change']
+        pc_value = float(percent_change[1:].strip('%'))
+
+        volume = float(res['volume'])
+        average_volume = float(res['average_volume'])
+        volume_ratio = volume/average_volume
+
+        print sym, percent_change, volume, average_volume, volume_ratio
+
+        if percent_change[0] == '+':
+            output_str = 'change is positive'
+            print output_str
+
+            if pc_value >= pc: 
+                output_str = 'pc_value = ' + str(pc_value) + ' is at least ' + str(pc)
+                print output_str
+
+                if volume_ratio >= vr: 
+                    output_str = 'volume_ratio = ' + str(volume_ratio) + ' is at least ' + str(vr)
+                    print output_str
+                    print 'Got a percent_change candidate'
+                    return [sym, percent_change, volume_ratio]
+                else:
+                    output_str = 'but volume_ration = ' + str(volume_ratio) + ' is less than ' + str(vr) + ', pass'
+                    print output_str
+            else:
+                output_str = 'pc_value = ' + str(pc_value) + ' is less than ' + str(pc) + ', pass'
+                print output_str
+        else:
+            output_str = 'negative change, pass'
+            print output_str
+
+        return None
+
+    def percent_change(self, pc, vr, symbol_list = None):
+
+        scan_symbol_list = []
+
+        if symbol_list != None:
+            for sym in symbol_list:
+                scan_symbol_list.append(sym)
+        else:
+            filelist = [ f for f in os.listdir(DATA_DIR) if f.endswith('.csv') ]
+            for f in filelist:
+                sym = f.split('.')[0]
+                scan_symbol_list.append(sym)
+
+        if len(scan_symbol_list) == 0: 
+            print 'Symbol list is empty, nothing to scan'
+
+        result = []
+        for sym in scan_symbol_list:
+            ret = self.percent_change_one_symbol(sym, pc, vr)
+            if ret != None: 
+                result.append(ret)
+            time.sleep(1)
+
+        if len(result) == 0: 
+            print 'Scan result emtpy'
+            return None
+        else:
+            for item in result:
+                print item
+
+            return result
