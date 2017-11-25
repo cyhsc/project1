1dbefd74963be70dab93ad92e2e3c1d2b04cd2d6
cyhscg2015 <cyhscg2015@gmail.com>
2017-11-25 07:33:26 -0800

    Add alpha vantage key

--- config.py
+++ config.py
@@ -2,10 +2,11 @@ CONFIG_DIR = '../config/'
 DATA_DIR = '../data/'
 ANALYSIS_DIR = '../analysis/'
 TRADABLE_STOCKS = '../data/tradable_stocks.txt'
-IBD_STOCKS = '../data/ibd_stocks.json'
+IBD_STOCKS = '../analysis/ibd_stocks.json'
 CUR_SYM = '../data/cur_symbol.txt'
 TWEETS_DIR = '../tweets/'
 SCRN_NAMES = TWEETS_DIR + 'scrn_names.txt'
+ALPHA_VANTAGE_KEY = CONFIG_DIR + 'alphavantage_key'
 
 ACCESS_TOKEN = 0
 ACCESS_SECRET = 1
@@ -15,3 +16,7 @@ CONSUMER_SECRET = 3
 def get_twitter_keys(id):
     lines = open(CONFIG_DIR + 'twitter_key', 'r').read().split('\n')
     return lines[id]
+
+def get_alphavantage_key():
+    lines = open(ALPHA_VANTAGE_KEY, 'r').read().split('\n')
+    return lines[0]
